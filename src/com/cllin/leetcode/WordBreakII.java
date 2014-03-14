0797427fbc134be37be75e40abd9f86bd7060e47
Ching-Lun Lin <clallenlin@gmail.com>
2014-03-14 01:22:27 -0700

    Word Break II
    Exercise from LeetCode, it is accepted

--- src/com/cllin/leetcode/WordBreakII.java
+++ src/com/cllin/leetcode/WordBreakII.java
@@ -2,7 +2,6 @@ package com.cllin.leetcode;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Hashtable;
 import java.util.Set;
 
 import com.cllin.main.LeetCodeExercise;
@@ -48,6 +47,31 @@ public class WordBreakII implements LeetCodeExercise {
 		dictionary.add("bbbb");
 		
 		testSuite.add(new TestCase(string, dictionary));
+		
+//		CASE 3
+		string = "aaaaaaa";
+		dictionary = new HashSet<String>();
+		dictionary.add("aaaa");
+		dictionary.add("aaa");
+		
+		testSuite.add(new TestCase(string, dictionary));
+		
+//		CASE 4
+		string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+		dictionary = new HashSet<String>();
+		
+		dictionary.add("aaaaaaaaaa");
+		dictionary.add("aaaaaaaaa");
+		dictionary.add("aaaaaaaa");
+		dictionary.add("aaaaaaa");
+		dictionary.add("aaaaaa");
+		dictionary.add("aaaaa");
+		dictionary.add("aaaa");
+		dictionary.add("aaa");
+		dictionary.add("aa");
+		dictionary.add("a");
+		
+		testSuite.add(new TestCase(string, dictionary));
 	}
 
 	@Override
@@ -67,41 +91,47 @@ public class WordBreakII implements LeetCodeExercise {
 			return solutions;
 		}
 		
-		Hashtable<String, Integer> queue = new Hashtable<String, Integer>();
-		Hashtable<String, String> solution = new Hashtable<String, String>();
+		int length = string.length();
+		ArrayList<ArrayList<String>> words = new ArrayList<ArrayList<String>>(length + 1);
 		
-		int index = 0;
-		while (index <= string.length()) {
-			String word = string.substring(0, index);
-			if (dictionary.contains(word)) {
-				queue.put(word, index);
-				solution.put(word, word);
-			}
-			
-			Set<String> keys = queue.keySet();
-			for (String key : keys) {
-				int start = queue.get(key);
-				String candidate = word.substring(start, index);
-				
-				if (dictionary.contains(candidate)) {
-					solution.put(key, solution.get(key) + " " + candidate);
-					queue.put(key, index);
-				}
-			}
-			
-			index++;
+		for (int i = 0; i < length + 1; i++) {
+			words.add(new ArrayList<String>());
 		}
 		
-		Set<String> keys = solution.keySet();
-		for (String key : keys) {
-			String s = solution.get(key);
-			if (s.replaceAll(" ", "").equals(string)) {
-				solutions.add(solution.get(key));
+		for (int i = length - 1; i >= 0; i--) {
+			for (int j = i; j < length; j++) {
+				String subString = string.substring(i, j + 1);
+
+				if (dictionary.contains(subString)) {
+					if (j == length - 1 || words.get(j + 1).size() != 0) {
+						words.get(i).add(subString);
+					}
+				}
+			}
+		}
+		
+		return buildString(string, 0, words);
+    }
+	
+	private ArrayList<String> buildString(String string, int index, ArrayList<ArrayList<String>> words) {
+		ArrayList<String> solutions = new ArrayList<String>();
+
+		for (String word : words.get(index)) {
+			if (index + word.length() == string.length()) {
+				solutions.add(word);
+			} else {
+				ArrayList<String> wd = buildString(string, index + word.length(), words);
+				
+				for (int i = 0; i < wd.size(); i++) {
+					wd.set(i, word + " " + wd.get(i));
+				}
+				
+				solutions.addAll(wd);
 			}
 		}
 		
 		return solutions;
-    }
+	}
 
 	@Override
 	public boolean test() {

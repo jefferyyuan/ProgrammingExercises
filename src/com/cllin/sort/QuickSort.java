cc175e4482c361fe70bd49a7c106ae434d56bd4a
Ching-Lun Lin <clallenlin@gmail.com>
2014-03-20 19:34:14 -0700

    Update QuickSort

--- src/com/cllin/sort/QuickSort.java
+++ src/com/cllin/sort/QuickSort.java
@@ -1,16 +1,27 @@
 package com.cllin.sort;
 
+/*
+ * Class							:Sorting algorithm
+ * Worst case performance			:O(n ^ 2)
+ * Best case performance			:O(n log n) (simple partition)
+ * Average case performance			:O(n log n)
+ * Worst case space complexity		:O(n)
+ *
+ * Reference						:http://en.wikipedia.org/wiki/Quicksort
+ */
+
 public class QuickSort extends Sort {
 	protected void sort(){
 		quickSort(0, SIZE - 1);
 	}
 	
-	private int partition(int start, int end){
+	private int partition(int start, int end) {
 		int x = numbers[end];
 		int i = start - 1;
 		
-		for(int j = start; j < end; j++){
-			if(numbers[j] <= x){
+//		Swap if the element is smaller than the last element
+		for (int j = start; j < end; j++) {
+			if (numbers[j] <= x) {
 				i++;
 				int tmp = numbers[j];
 				numbers[j] = numbers[i];
@@ -26,18 +37,19 @@ public class QuickSort extends Sort {
 	}
 	
 	@SuppressWarnings("unused")
-	private int randomizedPartition(int start, int end){
-		int i = (int)(Math.random() * (end -  start + 1)) + start;
+	private int randomizedPartition(int start, int end) {
+		int index = (int)(Math.random() * (end -  start + 1)) + start;
 		
-		int tmp = numbers[i];
-		numbers[i] = numbers[end];
+//		Swap the random element and the last element
+		int tmp = numbers[index];
+		numbers[index] = numbers[end];
 		numbers[end] = tmp;
 		
 		return partition(start, end);
 	}
 	
-	private void quickSort(int start, int end){
-		if(end > start){
+	private void quickSort(int start, int end) {
+		if (end > start) {
 			int partitionIndex = partition(start, end);
 //			int partitionIndex = randomizedPartition(start, end);
 			quickSort(start, partitionIndex - 1);

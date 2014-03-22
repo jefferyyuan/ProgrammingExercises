f9d717cfc9c957c4e212821cd03de5688df8efbc
Ching-Lun Lin <clallenlin@gmail.com>
2014-03-22 15:13:57 -0700

    ThisDemo
    Usage of "this"

---
+++ src/com/cllin/keywords/ThisDemo.java
@@ -0,0 +1,39 @@
+package com.cllin.keywords;
+
+import com.cllin.main.Exercise;
+
+public class ThisDemo implements Exercise {
+
+	@Override
+	public void runExercise() {
+		House h1 = new House(1, 1);
+		House h2 = new House(1);
+		House h3 = new House();
+		
+		h1.printHouse();
+		h2.printHouse();
+		h3.printHouse();
+	}
+
+	class House {
+		int nFloors;
+		int nRooms;
+		
+		House(int nFloors, int nRooms) {
+			this.nFloors = nFloors;
+			this.nRooms = nRooms;
+		}
+		
+		House(int nFloors) {
+			this(nFloors, 1);
+		}
+		
+		House() {
+			this(1, 1);
+		}
+		
+		void printHouse() {
+			System.out.printf("This house has %d floors and %d rooms%n", nFloors, nRooms);
+		}
+	}
+}

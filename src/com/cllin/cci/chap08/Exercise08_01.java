5a3937d55124e103bcf4db0edac8791fc399caba
Ching-Lun Lin <clallenlin@gmail.com>
2014-06-05 23:54:05 -0700

    Updates of 1 Exercise

--- src/com/cllin/cci/chap08/Exercise08_01.java
+++ src/com/cllin/cci/chap08/Exercise08_01.java
@@ -1,35 +1,37 @@
 package com.cllin.cci.chap08;
 
-import java.util.Arrays;
-
 import com.cllin.main.Exercise;
+
+/*
+ * Write a method to generate the nth Fibonacci number.
+ */
 
 public class Exercise08_01 implements Exercise {
 //	Limited by the size of an integer, can be extended by using a long instead
-	private final int MAXIMUM = 45;
-	private final int[] testSuite = {1, 2, 3, 5, 8, 15, 20, 30, 40, 45};
-	private int[] fibonacciNumbers;
 
 	@Override
 	public void runExercise() {
-		fibonacciNumbers = new int[MAXIMUM + 1];
-		Arrays.fill(fibonacciNumbers, -1);
-		Arrays.fill(fibonacciNumbers, 0, 3, 1);
-		
-		for(int n : testSuite){
+		for (int n = 1; n <= 45; n++) {
 			int fibonacci = getFibonacciNumber(n);
 			System.out.println("The " + n + "th Fibonacci number is " + fibonacci);
 		}
-
 	}
 	
-	private int getFibonacciNumber(int n){
+	private static int getFibonacciNumber(int n){
 		if (n == 1 || n == 2) return 1;
-		if (fibonacciNumbers[n] != -1) return fibonacciNumbers[n];
 		
-		fibonacciNumbers[n] = getFibonacciNumber(n - 2) + getFibonacciNumber(n - 1);
+		int last = 1;
+		int lastTwo = 1;
+		int number = last + lastTwo; 
+		int count = 3;
+		while (count < n) {
+			lastTwo = last;
+			last = number;
+			number = last + lastTwo;
+			count++;
+		}
 		
-		return fibonacciNumbers[n];
+		return number;
 	}
 
 }
